d62f16a59ec7fbd4a686a39cb737631a420a8bcd
daniel-souza <dssouzadan@gmail.com>
2021-07-01 07:12:21 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import app from './app.js'
 
-const PORT = process.env.APP_PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log("Servidor iniciado na porta 3000!");
